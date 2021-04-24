5305e81c5071513d424318a8fd699976cd15b735
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-04-24 12:04:10 -0300

    dia 1 grupo de estudos ms

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+import wateringImg from "../assets/watering.png";
+
+export function Welcome() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de {"\n"}
+          forma fácil
+        </Text>
+
+        <Image style={styles.image} source={wateringImg} resizeMode="contain" />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Text style={styles.buttonIcon}>
+            {'>'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 48,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+    marginTop: 30,
+  },
+  buttonIcon: {
+    fontSize: 28,
+    color: colors.white,
+  }
+});
